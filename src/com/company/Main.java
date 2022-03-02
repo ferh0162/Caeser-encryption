bac4547a133128e97f3ad4247207dde4f4657802
ferh0162 <ferh0162@stud.kea.dk>
2022-03-02 15:54:57 +0100

    Færdigt progam, kan både kryptere og dektryptere.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,29 +1,9 @@
 package com.company;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
-    public char stringTilChar(String tekstStringDerSkalKrypteres){
-        // Vi skal dele String op til bogstaver
-        // Derefter skal bogstavets tal findes
-        // Derefter skal tallet shiftes
-        // Efter shiftet skal tallet laves om til char igen
-            //Dette loopes og plusses sammen med det tidligere loop
-
-
-        String tekst = tekstStringDerSkalKrypteres;
-        // Vi skal dele String op til bogstaver
-        for (int perBogstav = 0; perBogstav < tekst.length(); perBogstav++){
-            String StringTekstTilBogstav = (tekst.substring(perBogstav,perBogstav+1));
-            //String bogstaver laves til Char bogstaver
-            char StringBogstavTilChar = StringTekstTilBogstav.charAt(0);
-            charTilInt(StringBogstavTilChar);
-        }
-
-        char bogstav;
-
-        return 0;
-    }
     public int charTilInt(char bogstav) {
         //Der skal laves en metode, som modtager en char og laver den om til tal i alfabetet
 
@@ -36,12 +16,21 @@ public class Main {
         // Hvis den finder et match så skal den skrive matchets index tal ud
         for (int i = 0; alfabetet.length >= i; i++) {
             if (alfabetet[i] == bogstav) {
-                System.out.println(i);
                 return i;
             }
         }
         return 0;
     }
+
+    public int shiftInt(int talDerSkalShiftes, int shift) {
+        int shiftetTal = talDerSkalShiftes + shift;
+        if (shiftetTal<0){
+            shiftetTal = 29 + shiftetTal; //29 er alfabetets længde
+            return shiftetTal;
+        }
+        return shiftetTal;
+    }
+
     public char inttilChar(int index) {
         // Vi skal kunne modtage små bogstaver og laver dem alle til store bogstaver
         // Vi skal finde en måde at lave char til .toUpperCase
@@ -51,22 +40,112 @@ public class Main {
         return indexNummer;
     }
 
-    public static void main(String[] args) {
-	// write your code here
-
-        //Velkommen
-        System.out.println("Velkommen til Ferhat krypteringsprogram!");
-        System.out.println();
-
-        // Få brugeren til at indtaste tekst som skal krypteres
+    public void krypterTekst(){
         Scanner sc = new Scanner(System.in);
         System.out.print("Indtast tekst som skal krypteres: ");
-        String tekstTilKryption = sc.nextLine();
+        String tekstStringDerSkalKrypteres = sc.nextLine();
+        tekstStringDerSkalKrypteres = tekstStringDerSkalKrypteres.toUpperCase(Locale.ROOT);
         System.out.println();
         System.out.print("Indtast tal, som teksten skal shiftes med: ");
-        int talTilShift = sc.nextInt();
+        int shift = sc.nextInt();
         System.out.println();
 
-        //Lav en metode som kan tage tekstTilKryption og finde deres index værdi i alfabetet
+        Main obj = new Main();
+        System.out.println("Denne tekst: ");
+        System.out.println(tekstStringDerSkalKrypteres);
+        System.out.println("Bliver til dette: ");
+
+
+        String tekst = tekstStringDerSkalKrypteres;
+        // Vi skal dele String op til bogstaver
+        for (int perBogstav = 0; perBogstav < tekst.length(); perBogstav++) {
+
+            //Her spytter den per bogstav ud som en String
+            String StringTekstTilBogstav = (tekst.substring(perBogstav, perBogstav + 1));
+
+            //String bogstaver laves til Char bogstaver
+            char StringBogstavTilChar = StringTekstTilBogstav.charAt(0);
+
+            // Laver en char om til int
+            int tal = obj.charTilInt(StringBogstavTilChar);
+
+            // vi Shifter Int
+            int shiftetTal = obj.shiftInt(tal, shift);
+
+            // Tage det shiftede Int og lav det om til en Char
+            char krypteretTekst = obj.inttilChar(shiftetTal);
+
+
+            // konkatenere char
+            System.out.print(krypteretTekst);
+            //Lav en metode som kan tage tekstTilKryption og finde deres index værdi i alfabetet
+        }
+    }
+
+    public void dekrypterTekst(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Indtast tekst som skal dekrypteres: ");
+        String tekstStringDerSkalKrypteres = sc.nextLine();
+        tekstStringDerSkalKrypteres = tekstStringDerSkalKrypteres.toUpperCase(Locale.ROOT);
+        System.out.println();
+        System.out.print("Indtast tal, som teksten skal shiftes med: ");
+        int shift = sc.nextInt();
+        shift = shift*-1; //Shift ændres
+        System.out.println();
+
+        Main obj = new Main();
+        System.out.println("Denne tekst: ");
+        System.out.println(tekstStringDerSkalKrypteres);
+        System.out.println("Bliver til dette: ");
+
+
+        String tekst = tekstStringDerSkalKrypteres;
+        // Vi skal dele String op til bogstaver
+        for (int perBogstav = 0; perBogstav < tekst.length(); perBogstav++) {
+
+            //Her spytter den per bogstav ud som en String
+            String StringTekstTilBogstav = (tekst.substring(perBogstav, perBogstav + 1));
+
+            //String bogstaver laves til Char bogstaver
+            char StringBogstavTilChar = StringTekstTilBogstav.charAt(0);
+
+            // Laver en char om til int
+            int tal = obj.charTilInt(StringBogstavTilChar);
+
+            // vi Shifter Int
+            int shiftetTal = obj.shiftInt(tal, shift);
+
+            // Tage det shiftede Int og lav det om til en Char
+            char krypteretTekst = obj.inttilChar(shiftetTal);
+
+
+            // konkatenere char
+            System.out.print(krypteretTekst);
+            //Lav en metode som kan tage tekstTilKryption og finde deres index værdi i alfabetet
+        }
+    }
+
+    public static void main(String[] args) {
+        // write your code here
+        Main obj = new Main();
+        Scanner sc = new Scanner(System.in);
+
+        //Velkommen
+        System.out.println("Velkommen til Ferhats krypteringsprogram!");
+        System.out.println();
+
+        System.out.println("Vil du kryptere eller dekryptere en tekst?");
+        System.out.println("Skriv K eller D");
+        String brugerValg = sc.nextLine();
+        brugerValg = brugerValg.toUpperCase(Locale.ROOT);
+
+        if (brugerValg == "S"){
+            obj.krypterTekst();
+        } else{
+            obj.dekrypterTekst();
+        }
+
+        // Få brugeren til at indtaste tekst som skal krypteres
+
     }
 }
